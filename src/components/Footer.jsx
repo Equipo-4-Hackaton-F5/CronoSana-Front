78afaf10b25cb68bfd47a8d335c1172d944480f1
olgararo <olgararo1402@gmail.com>
2025-09-18 17:15:43 +0200

    fix: errata

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
             </footer>
 
             {/* espaciador para que el contenido no quede debajo del footer fijo */}
-            <adiv style={{ height: '60px' }} />
+            <div style={{ height: '60px' }} />
         </>
     );
 };
